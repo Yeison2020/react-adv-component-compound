9cc956f7b630b460c9e2f3d9a90302152d67aee2
Yeison <enmanuelquiterios@gmail.com>
2022-04-06 11:32:03 -0400

    Updates

--- src/02-component-patterns/interfaces/interfaces.ts
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -1,4 +1,4 @@
-import { Props } from "../components/ProductCard";
+import { Props as ProductCardProps } from "../components/ProductCard";
 
 export interface Product {
   id: string;
@@ -13,7 +13,7 @@ export interface ProductContextProps {
 }
 
 export interface ProductCardHOCProps {
-  ({ children, product }: Props): JSX.Element;
+  ({ children, product }: ProductCardProps): JSX.Element;
   Title: ({ title }: { title?: string }) => JSX.Element;
   Image: ({ img }: { img?: string }) => JSX.Element;
   Buttons: () => JSX.Element;
