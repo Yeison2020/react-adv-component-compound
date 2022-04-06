1fae8ffb80b0f0356101b01be8f56251dcbb9398
Yeison <enmanuelquiterios@gmail.com>
2022-04-06 11:28:34 -0400

    Updating componet to custom their css

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,27 +1,29 @@
-import { createContext } from 'react';
+import { createContext, ReactElement } from "react";
 
-import { useProduct } from '../hooks/useProduct';
-import { ProductContextProps, ProductCardProps } from '../interfaces/interfaces';
+import { useProduct } from "../hooks/useProduct";
+import { ProductContextProps, Product } from "../interfaces/interfaces";
 
-import styles from '../styles/styles.module.css'
+import styles from "../styles/styles.module.css";
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
-
-
-export const ProductCard = ({ children, product }: ProductCardProps ) => {
-
-    const { counter, increaseBy } = useProduct();
-
-    return (
-        <Provider value={{
-            counter,
-            increaseBy,
-            product
-        }}>
-            <div className={ styles.productCard }>
-                { children }
-            </div>
-        </Provider>
-    )
+export interface Props {
+  product: Product;
+  children?: ReactElement | ReactElement[];
 }
+
+export const ProductCard = ({ children, product }: Props) => {
+  const { counter, increaseBy } = useProduct();
+
+  return (
+    <Provider
+      value={{
+        counter,
+        increaseBy,
+        product,
+      }}
+    >
+      <div className={styles.productCard}>{children}</div>
+    </Provider>
+  );
+};
--- src/02-component-patterns/interfaces/interfaces.ts
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -1,29 +1,20 @@
-import { ReactElement } from 'react';
-
-
-export interface ProductCardProps {
-    product: Product;
-    children?: ReactElement | ReactElement[] 
-}
+import { Props } from "../components/ProductCard";
 
 export interface Product {
-    id: string;
-    title: string;
-    img?: string;
+  id: string;
+  title: string;
+  img?: string;
 }
 
 export interface ProductContextProps {
-    counter: number;
-    increaseBy: ( value: number ) => void;
-    product: Product;
+  counter: number;
+  increaseBy: (value: number) => void;
+  product: Product;
 }
-
 
 export interface ProductCardHOCProps {
-    ({ children, product }: ProductCardProps ):JSX.Element,
-    Title: ({ title }: { title?: string }) => JSX.Element,
-    Image: ({ img }: { img?: string }) => JSX.Element,
-    Buttons: () => JSX.Element
+  ({ children, product }: Props): JSX.Element;
+  Title: ({ title }: { title?: string }) => JSX.Element;
+  Image: ({ img }: { img?: string }) => JSX.Element;
+  Buttons: () => JSX.Element;
 }
-
-
