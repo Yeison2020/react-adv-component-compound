3816b746f7674f8b5776e3196e3b1c9626e8aa62
Yeison <enmanuelquiterios@gmail.com>
2022-04-07 21:45:58 -0400

    Parents Cmm

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -7,6 +7,8 @@ import styles from "../styles/styles.module.css";
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
+
+// Parent Values
 export interface Props {
   product: Product;
   children?: ReactElement | ReactElement[];
