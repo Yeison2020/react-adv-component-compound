94f22f65c9d87851a03e11a5e21916bed4c4f5d7
Yeison <enmanuelquiterios@gmail.com>
2022-04-07 21:44:04 -0400

    Comments

--- src/02-component-patterns/components/index.ts
+++ src/02-component-patterns/components/index.ts
@@ -1,21 +1,20 @@
-import { ProductCard as ProductCardHOC } from './ProductCard';
-import { ProductCardHOCProps } from '../interfaces/interfaces';
+// Here are all my importantions
 
-import { ProductButtons } from './ProductButtons';
-import { ProductImage } from './ProductImage';
-import { ProductTitle } from './ProductTitle';
+import { ProductCard as ProductCardHOC } from "./ProductCard";
+import { ProductCardHOCProps } from "../interfaces/interfaces";
 
-export { ProductButtons } from './ProductButtons';
-export { ProductImage } from './ProductImage';
-export { ProductTitle } from './ProductTitle';
+import { ProductButtons } from "./ProductButtons";
+import { ProductImage } from "./ProductImage";
+import { ProductTitle } from "./ProductTitle";
 
+export { ProductButtons } from "./ProductButtons";
+export { ProductImage } from "./ProductImage";
+export { ProductTitle } from "./ProductTitle";
 
-export const ProductCard: ProductCardHOCProps = Object.assign( ProductCardHOC, {
-    Title: ProductTitle,
-    Image: ProductImage,
-    Buttons: ProductButtons
-})
-
+export const ProductCard: ProductCardHOCProps = Object.assign(ProductCardHOC, {
+  Title: ProductTitle,
+  Image: ProductImage,
+  Buttons: ProductButtons,
+});
 
 export default ProductCard;
-
